4ca312314fccb9c07247e8368092e9cda2d67b8e
endop <endopedro@gmail.com>
2020-01-28 23:10:36 -0300

    Front page header layout

---
+++ web/app/themes/evaldomariano/resources/views/front-page.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+  @include('partials.sections.front-header')
+@endsection
---
+++ web/app/themes/evaldomariano/resources/views/partials/sections/front-header.blade.php
@@ -0,0 +1,20 @@
+<section class="front-header">
+
+  <div class="slide-item" style="background: url('http://evaldomariano.com.br/wp-content/uploads/cropped-ev_avt.jpg') repeat scroll right center / cover"></div>
+
+  <div class="container">
+    <div class="front-header-content">
+      <h1 class="header-title">EVALDO MARIANO</h1>
+      <h2 class="header-subtitle"><span class="typed">texto do typed</span></h2>
+
+      <ul class="list-unstyled header-social">
+        <li><a href="#"><i class="ion-social-facebook"></i>A</a></li>
+        <li><a href="#"><i class="ion-social-instagram"></i>A</a></li>
+        <li><a href="#"><i class="fab fa-youtube"></i>A</a></li>
+        <li><a href="http://api.whatsapp.com/send?1=pt_BR&phone=#"><i class="fab fa-whatsapp"></i>A</a></li>
+      </ul>
+
+    </div>
+  </div>
+
+</section>
