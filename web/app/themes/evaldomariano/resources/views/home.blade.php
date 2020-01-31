629ad033e341b39df87ac84c5f6cb7846c4f6d32
endop <endopedro@gmail.com>
2020-01-30 22:01:19 -0300

    Services section

--- web/app/themes/evaldomariano/resources/views/home.blade.php
+++ web/app/themes/evaldomariano/resources/views/home.blade.php
@@ -4,4 +4,5 @@
   @include('partials.sections.home-header')
   @include('partials.sections.about')
   @include('partials.sections.symptoms')
+  @include('partials.sections.services')
 @endsection
---
+++ web/app/themes/evaldomariano/resources/views/partials/sections/services.blade.php
@@ -0,0 +1,20 @@
+<section class="section-services">
+  <div class="container">
+
+    <div class="services-title">
+      <h2>O que ofereço</h2>
+    </div>
+
+    <div class="services-icons-wrap">
+
+      <a href="#">
+        <div class="services-icon">
+          <img src="@asset('images/icons/idea.png')" alt="">
+          <span>Orientação Psicológica</span>
+        </div>
+      </a>
+
+    </div>
+
+  </div>
+</section>
