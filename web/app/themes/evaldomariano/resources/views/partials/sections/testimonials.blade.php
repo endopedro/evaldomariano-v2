4c051be9b181240babb5a440faa75728d75d7b65
endop <endopedro@gmail.com>
2020-02-20 15:09:19 -0300

    Section testimonials

--- web/app/themes/evaldomariano/resources/views/partials/sections/testimonials.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/testimonials.blade.php
@@ -1,3 +1,18 @@
-<section class="section-testimonials">
+<section class="section section-testimonials">
+  <div class="container">
 
+    <div class="section-title">
+      <h2>Depoimentos</h2>
+    </div>
+
+    <h3 class="testimonials-subtitle">Alguns relatos de pessoas que recomendam meus serviços</h3>
+    <img class="testimonials-image" src="@asset('images/testimonial.png')" alt="">
+
+    <div class="testimonials-testimonial">
+      <p class="testimonial-text">“Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro.”</p>
+      <h3 class="testimonial-author">Ana Beatriz</h3>
+      <h4 class="testimonial-author-profession">Ex-Aluna</h4>
+    </div>
+
+  </div>
 </section>
