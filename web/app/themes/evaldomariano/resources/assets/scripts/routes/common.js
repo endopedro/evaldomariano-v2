ce37d67bc6cfb3221e1afbdcb397c9737b3e8c92
endop <endopedro@gmail.com>
2020-02-29 15:24:53 -0300

    Add slick carousel to home header

--- web/app/themes/evaldomariano/resources/assets/scripts/routes/common.js
+++ web/app/themes/evaldomariano/resources/assets/scripts/routes/common.js
@@ -1,6 +1,23 @@
+import 'slick-carousel'
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+    // Home header carousel
+    const membersSlickCarousel = $('.home-header-carousel.slick-carousel')
+    if (membersSlickCarousel !== null) {
+      membersSlickCarousel.slick({
+        infinite: true,
+        speed: 1000,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        fade: true,
+        autoplaySpeed: 7000,
+        adaptiveHeight: true,
+      })
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- web/app/themes/evaldomariano/resources/views/partials/sections/home-header.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/home-header.blade.php
@@ -1,15 +1,20 @@
 
 @php
-    $images_sliders = get_field('header_slider');
+  $images_sliders = get_field('header_slider');
 @endphp
+
 <section class="home-header">
 
-  <div class="slide-item" style="background: url('http://evaldomariano.com.br/wp-content/uploads/cropped-ev_avt.jpg') repeat scroll right center / cover"></div>
+  <div class="home-header-carousel slick-carousel">
+    <img class="home-header-carousel-image" src="http://evaldomariano.com.br/wp-content/uploads/cropped-ev_avt.jpg" alt="">
+    <img class="home-header-carousel-image" src="http://evaldomariano.com.br/wp-content/uploads/IMG_1857-1.jpeg" alt="">
+    <img class="home-header-carousel-image" src="http://evaldomariano.com.br/wp-content/uploads/91289a26-8cbb-402e-ae4a-72402dcfe8f8-1024x768.jpg" alt="">
+  </div>
 
   <div class="container">
     <div class="home-header-content">
       <h1 class="header-title">{{ the_field('header_title' )}} </h1>
-    <h2 class="header-subtitle"><span class="typed">{{the_field('header_caption')}}</span></h2>
+    <h2 class="header-subtitle"><span class="typed">{{ the_field('header_caption') }}</span></h2>
 
       <ul class="list-unstyled header-social">
         <li><a href="#"><i class="ion-social-facebook"></i>A</a></li>
