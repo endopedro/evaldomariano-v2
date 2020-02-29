13fe800afbed239e67007ffd0c445de06b820c7e
endop <endopedro@gmail.com>
2020-02-29 16:37:52 -0300

    Add typedjs to home header

--- web/app/themes/evaldomariano/resources/assets/scripts/routes/common.js
+++ web/app/themes/evaldomariano/resources/assets/scripts/routes/common.js
@@ -1,4 +1,5 @@
 import 'slick-carousel'
+import Typed from 'typed.js';
 
 export default {
   init() {
@@ -18,6 +19,16 @@ export default {
         adaptiveHeight: true,
       })
     }
+
+    // Home header Typed
+    new Typed('#typed', {
+      strings: [$('#typed-text').text()],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true,
+      backDelay: 2000,
+    });
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- web/app/themes/evaldomariano/resources/views/partials/sections/home-header.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/home-header.blade.php
@@ -14,14 +14,15 @@
   <div class="container">
     <div class="home-header-content">
       <h1 class="header-title">{{ the_field('header_title' )}} </h1>
-    <h2 class="header-subtitle"><span class="typed">{{ the_field('header_caption') }}</span></h2>
+      <h2 class="header-subtitle"><span id="typed"></span></h2>
+      <div class="d-none" id="typed-text">{{ the_field('header_caption') }}</div>
 
-      <ul class="list-unstyled header-social">
-        <li><a href="#"><i class="ion-social-facebook"></i>A</a></li>
-        <li><a href="#"><i class="ion-social-instagram"></i>A</a></li>
-        <li><a href="#"><i class="fab fa-youtube"></i>A</a></li>
-        <li><a href="http://api.whatsapp.com/send?1=pt_BR&phone=#"><i class="fab fa-whatsapp"></i>A</a></li>
-      </ul>
+      <div class="header-social list-unstyled">
+        <a class="header-social-item" href="#"><i class="fab fa-facebook-square"></i></a>
+        <a class="header-social-item" href="#"><i class="fab fa-instagram"></i></a>
+        <a class="header-social-item" href="#"><i class="fab fa-youtube"></i></a>
+        <a class="header-social-item" href="http://api.whatsapp.com/send?1=pt_BR&phone=#"><i class="fab fa-whatsapp"></i></a>
+      </div>
 
     </div>
   </div>
