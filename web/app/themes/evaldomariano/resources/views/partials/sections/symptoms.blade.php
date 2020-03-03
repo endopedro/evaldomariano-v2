fe3c7d73684f9c663942d6aa64da27bbd9c21d73
endop <endopedro@gmail.com>
2020-03-03 10:54:22 -0300

    Symptoms section fonts adjustment

--- web/app/themes/evaldomariano/resources/views/partials/sections/symptoms.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/symptoms.blade.php
@@ -1,12 +1,12 @@
-<section class="section section-symptoms" style="background: url('@asset('images/consultorio.jpg')') repeat scroll center center / cover;">
+<section class="section section-symptoms">
   <div class="container">
     <div class="row align-items-center">
-      <div class="col-md-6">
-        <h5 class="symptoms-left-title">HÁ ALGUÉM QUE VOCÊ PODE DESABAFAR</h5>
-        <h2 class="symptoms-left-subtitle">Muito Além do Ouvir…</h2>
+      <div class="col-md-6 left">
+        <h5 class="symptoms-title">HÁ ALGUÉM QUE VOCÊ PODE DESABAFAR</h5>
+        <h2 class="symptoms-subtitle">Muito Além do Ouvir…</h2>
         <h3 class="symptoms-text">POSSO TE AJUDAR<br>Caso você esteja sentindo...</h3>
       </div>
-      <div class="col-md-6 lado-b">
+      <div class="col-md-6 right">
         <p class="symptoms-text">
           → Ansiedade, Depressão e Pânico<br><br>
           → Tristeza, Estresse e Falta de Motivação<br><br>
