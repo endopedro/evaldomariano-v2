95acba9b6f40be926f82073e2f64fa15ed45c99c
endop <endopedro@gmail.com>
2020-03-07 12:16:56 -0300

    Section testimonials carousel

--- web/app/themes/evaldomariano/resources/assets/scripts/routes/home.js
+++ web/app/themes/evaldomariano/resources/assets/scripts/routes/home.js
@@ -1,6 +1,45 @@
+import 'slick-carousel'
+import Typed from 'typed.js';
+
 export default {
   init() {
     // JavaScript to be fired on the home page
+
+    // Home header carousel
+    const membersSlickCarousel = $('.home-header-carousel.slick-carousel')
+    if (membersSlickCarousel !== null) {
+      membersSlickCarousel.slick({
+        infinite: true,
+        speed: 1000,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        fade: true,
+        autoplaySpeed: 7000,
+        adaptiveHeight: true,
+      })
+    }
+
+    // Section testimonials carousel
+    const testimonialsSlickCarousel = $('.testimonials-carousel.slick-carousel')
+    if (testimonialsSlickCarousel !== null) {
+      testimonialsSlickCarousel.slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 10000,
+      });
+    }
+
+    // Home header Typed
+    new Typed('#typed', {
+      strings: [$('#typed-text').text()],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true,
+      backDelay: 2000,
+    });
+
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
--- web/app/themes/evaldomariano/resources/views/partials/sections/testimonials.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/testimonials.blade.php
@@ -8,10 +8,32 @@
     <h3 class="testimonials-subtitle">Alguns relatos de pessoas que recomendam meus serviços</h3>
     <img class="testimonials-image" src="@asset('images/testimonial_ic.png')" alt="">
 
-    <div class="testimonials-testimonial">
-      <p class="testimonial-text">“Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro.”</p>
-      <h3 class="testimonial-author">Ana Beatriz</h3>
-      <h4 class="testimonial-author-profession">Ex-Aluna</h4>
+    <div class="testimonials-carousel slick-carousel">
+
+      <div class="testimonials-testimonial">
+        <p class="testimonial-text">“Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro.”</p>
+        <h3 class="testimonial-author">Ana Beatriz</h3>
+        <h4 class="testimonial-author-profession">Ex-Aluna</h4>
+      </div>
+
+      <div class="testimonials-testimonial">
+        <p class="testimonial-text">“Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro. Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro. ”</p>
+        <h3 class="testimonial-author">Ana Beatriz</h3>
+        <h4 class="testimonial-author-profession">Ex-Aluna</h4>
+      </div>
+
+      <div class="testimonials-testimonial">
+        <p class="testimonial-text">“Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro.”</p>
+        <h3 class="testimonial-author">Ana Beatriz</h3>
+        <h4 class="testimonial-author-profession">Ex-Aluna</h4>
+      </div>
+
+      <div class="testimonials-testimonial">
+        <p class="testimonial-text">“Somos gratos ao Dr. Evaldo Mariano por nos aconselhar nos momentos difíceis. Suas orientações nos ajudaram a fortalecer o nosso relacionamento. Foi através dele que passamos a nos conhecer melhor e compreender mais um ao outro.”</p>
+        <h3 class="testimonial-author">Ana Beatriz</h3>
+        <h4 class="testimonial-author-profession">Ex-Aluna</h4>
+      </div>
+
     </div>
 
   </div>
