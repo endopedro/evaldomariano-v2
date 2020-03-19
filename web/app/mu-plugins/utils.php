630dab623440863d53efdbfb8c87624227eea83a
fsnay <naaypf@gmail.com>
2020-03-19 00:10:48 -0300

    adding testimonials post-type

---
+++ web/app/mu-plugins/utils.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'content-types/services/post-type.php';
+include 'content-types/services/config.php';
+include 'content-types/testimonials/post-type.php';
+include 'content-types/testimonials/config.php';
