5db11ab51364c0a305f29a04120c153839f2367c
Pedro Endo <endopedro@gmail.com>
2020-09-16 00:02:49 -0300

    fix: about section adjustments

--- web/app/themes/evaldomariano/resources/views/partials/sections/about.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/about.blade.php
@@ -1,30 +1,33 @@
 @php
-    $aboutme = get_field('personal_info');
+  $aboutme = get_field('personal_info');
 @endphp
 
 <section class="section section-about">
   <div class="container">
     <div class="row justify-content-between">
+
       <div class="col-lg-4 mb-3 mb-lg-0">
         <div class="about-img-frame">
           <div class="about-img">
-            {{-- <img src="http://evaldomariano.com.br/wp-content/uploads/cropped-ev_avt.jpg" class="img-responsive" alt="me"> --}}
-          <img src="{{$aboutme['personal_img']}}" class="img-responsive animated fadeInLeft" alt="me">
+            @if ($aboutme['personal_img'])
+              <img src="{{ $aboutme['personal_img'] }}" alt="foto">
+            @endif
           </div>
         </div>
       </div>
 
       <div class="col-lg-7">
-        <div class="about-descr">
-          <p class="about-descr-heading animated fadeInRight">
+        <div class="about-description">
+          <p class="about-description-heading">
             {!! $aboutme['personal_title'] !!}<br>
             {!! $aboutme['personal_caption'] !!}
           </p>
-          <div class="about-descr-text">
+          <div class="about-description-text">
             {!! $aboutme['personal_content'] !!}
           </div>
         </div>
       </div>
+
     </div>
   </div>
 </section>
