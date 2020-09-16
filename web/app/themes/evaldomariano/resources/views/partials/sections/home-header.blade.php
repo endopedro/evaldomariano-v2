9ec50aead6152eeacc2c308c94ea99443d2c4d3d
Pedro Endo <endopedro@gmail.com>
2020-09-15 21:57:02 -0300

    fix: home-header adjustments

--- web/app/themes/evaldomariano/resources/views/partials/sections/home-header.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/sections/home-header.blade.php
@@ -7,16 +7,14 @@
 <section class="home-header">
 
   <div class="home-header-carousel slick-carousel">
-    @php
-      foreach($header_fields['header_slider'] as $img){
-        echo '<img class="home-header-carousel-image" src="' . $img .'" alt="">' ;
-      }
-    @endphp
+    @foreach($header_fields['header_slider'] as $img)
+      <img class="home-header-carousel-image" src="{{ $img }}" alt="">
+    @endforeach
   </div>
 
   <div class="container">
     <div class="home-header-content">
-      <h1 class="header-title">{{ $header_fields['header_title']}} </h1>
+      <h1 class="header-title">{{ $header_fields['header_title'] }}</h1>
       <h2 class="header-subtitle"><span id="typed"></span></h2>
       <div class="d-none" id="typed-text">{{ $header_fields['header_caption'] }}</div>
 
@@ -31,7 +29,7 @@
           <a class="header-social-item" href="{{ $social_fields['yt_link'] }}"><i class="fab fa-youtube"></i></a>
         @endif
         @if($social_fields['wpp_contact'])
-          <a class="header-social-item" href="http://api.whatsapp.com/send?1=pt_BR&phone=+55{{ $social_fields['wpp_contact']}}"><i class="fab fa-whatsapp"></i></a>
+          <a class="header-social-item" href="http://api.whatsapp.com/send?1=pt_BR&phone=+55{{ $social_fields['wpp_contact'] }}"><i class="fab fa-whatsapp"></i></a>
         @endif
       </div>
 
