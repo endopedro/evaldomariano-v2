3bae862e8f64bb78af6789d20e910a4469322853
Pedro Endo <endopedro@gmail.com>
2021-04-21 22:59:49 -0300

    fix layout spacings

--- web/app/themes/evaldomariano/resources/views/partials/footer.blade.php
+++ web/app/themes/evaldomariano/resources/views/partials/footer.blade.php
@@ -7,37 +7,33 @@
 
 <footer class="footer">
   <div class="container">
-    <div class="row">
+    <div class="row contact">
       <div class="col-lg-4">
-        <div class="contact">
-          <h2 class="contact-title">Entre em contato</h2>
-          @includeWhen($info['contact_address'], '../components/contact-item', [
-            'content' => $info['contact_address'],
-            'icon' => 'fas fa-map-marker-alt'
-          ])
-          @includeWhen($info['contact_phone_number'], '../components/contact-item', [
-            'content' => $info['contact_phone_number'],
-            'icon' => 'fas fa-phone'
-          ])
-          @includeWhen($info['contact_open_hours'], '../components/contact-item', [
-            'content' => $info['contact_open_hours'],
-            'icon' => 'far fa-clock'
-          ])
-          @includeWhen($info['contact_whatsapp'], '../components/contact-item', [
-            'content' => $info['contact_whatsapp'],
-            'icon' => 'fab fa-whatsapp'
-          ])
-          @includeWhen($info['contact_email'], '../components/contact-item', [
-            'content' => $info['contact_email'],
-            'icon' => 'far fa-envelope'
-          ])
-        </div>
+        <h2 class="contact-title">Entre em contato</h2>
+        @includeWhen($info['contact_address'], '../components/contact-item', [
+          'content' => $info['contact_address'],
+          'icon' => 'fas fa-map-marker-alt'
+        ])
+        @includeWhen($info['contact_phone_number'], '../components/contact-item', [
+          'content' => $info['contact_phone_number'],
+          'icon' => 'fas fa-phone'
+        ])
+        @includeWhen($info['contact_open_hours'], '../components/contact-item', [
+          'content' => $info['contact_open_hours'],
+          'icon' => 'far fa-clock'
+        ])
+        @includeWhen($info['contact_whatsapp'], '../components/contact-item', [
+          'content' => $info['contact_whatsapp'],
+          'icon' => 'fab fa-whatsapp'
+        ])
+        @includeWhen($info['contact_email'], '../components/contact-item', [
+          'content' => $info['contact_email'],
+          'icon' => 'far fa-envelope'
+        ])
       </div>
       <div class="col-lg-6 offset-lg-2">
-        <div class="contact">
-          <div class="contact-form">
-            {!! do_shortcode('[contact-form-7 id="1707" title="Contact form 1"]') !!}
-          </div>
+        <div class="contact-form mt-4 mt-lg-0">
+          {!! do_shortcode('[contact-form-7 id="1707" title="Contact form 1"]') !!}
         </div>
       </div>
     </div>
