ecabdec08d17f48415879b6850f7eb9b4e86a933
Pedro Endo <endopedro@gmail.com>
2021-05-08 13:03:16 -0300

    fix body wrapper and spacing

--- web/app/themes/evaldomariano/resources/views/layouts/app.blade.php
+++ web/app/themes/evaldomariano/resources/views/layouts/app.blade.php
@@ -2,23 +2,25 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    {{-- <div class="wrap{{ is_front_page() ? "" : " container" }}" role="document"> --}}
-    <div class="wrap" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+    <div class="body-wrapper">
+      @php do_action('get_header') @endphp
+      @include('partials.header')
+      {{-- <div class="wrap{{ is_front_page() ? "" : " container" }}" role="document"> --}}
+      <div class="wrap" role="document">
+        <div class="content">
+          <main class="main">
+            @yield('content')
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+        </div>
       </div>
+      @php do_action('get_footer') @endphp
+      @include('partials.footer')
+      @php wp_footer() @endphp
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
   </body>
 </html>
